8140843ef86a37f0ebbb88e9cc6ccfcd3a1f8c6f
Myron Marintsev <marintsev@gmail.com>
2016-01-28 04:00:06 +0300

    Проект eclipse.

--- commands.c
+++ commands.c
@@ -2,82 +2,69 @@
 
 #include "entry.h"
 
-void read_word( char * word, int size )
-{
-    printf( "Введите слово> " );
-    fgets( word, size, stdin );
-    remove_newline( word );
-    // TODO: не позволять пустое слово
-    assert( strlen( word ) != 0 );
+void read_word(char * word, int size) {
+	printf("Введите слово> ");
+	fgets(word, size, stdin);
+	remove_newline(word);
+	// TODO: не позволять пустое слово
+	assert(strlen(word) != 0);
 }
 
-void read_content( char * content, int size )
-{
-	printf( "Введите толкование > " );
+void read_content(char * content, int size) {
+	printf("Введите толкование > ");
 	// TODO: ввод множества строк
-	fgets( content, 4096, stdin );
-	remove_newline( content );
+	fgets(content, 4096, stdin);
+	remove_newline(content);
 }
 
-void command_add( FILE * f )
-{
-    char word[130];
-	read_word( word, 130 );
+void command_add(FILE * f) {
+	char word[130];
+	read_word(word, 130);
 
-	uint64 index = find_word( f, word );
-	if( 0 == index )
-	{
+	uint64 index = find_word(f, word);
+	if (0 == index) {
 		// Слова нет, можно добавлять
-		char content[4096+8];
-		read_content( content, 4096 );
-		add_word( f, word, content );
-	}
-	else
-	{
+		char content[4096 + 8];
+		read_content(content, 4096);
+		add_word(f, word, content);
+	} else {
 		// найденное слово запомним
 		struct entry_t entry;
-		read_entry( f, &entry, index, READ_ENTRY_WORD | READ_ENTRY_DO_SEEK );
-		remove_words( f, entry.word );
-		char content[4096+8];
-		read_content( content, 4096 );
-		add_word( f, entry.word, content );
-		free_entry( &entry );
+		read_entry(f, &entry, index, READ_ENTRY_WORD | READ_ENTRY_DO_SEEK);
+		remove_words(f, entry.word);
+		char content[4096 + 8];
+		read_content(content, 4096);
+		add_word(f, entry.word, content);
+		free_entry(&entry);
 
 		//TODO( "Прочитать толкование, запомнить слово, вставить новую, удалить старую." );
 	}
 }
 
-void command_find( FILE * f )
-{
+void command_find(FILE * f) {
 	char word[130];
-	read_word( word, 130 );
+	read_word(word, 130);
 
-	uint64 index = find_word( f, word );
-	if( 0 == index )
-	{
+	uint64 index = find_word(f, word);
+	if (0 == index) {
 		// слова нет, сообщить
-		fprintf( stderr, "[INFO] нет такого слова\n" );
-	}
-	else
-	{
+		fprintf( stderr, "[INFO] нет такого слова\n");
+	} else {
 		// слово есть, выводим
 		//fseek( f, index, SEEK_SET );
 		struct entry_t entry;
-		read_entry( f, &entry, index, READ_ENTRY_CONTENT | READ_ENTRY_DO_SEEK );
-		printf( "Значение: %s\n", entry.content );
+		read_entry(f, &entry, index, READ_ENTRY_CONTENT | READ_ENTRY_DO_SEEK);
+		printf("Значение: %s\n", entry.content);
 	}
 }
 
-int remove_words( FILE * f, char * word )
-{
+int remove_words(FILE * f, char * word) {
 	int count = 0;
 	// асимптотика квадратичная :)
 	// TODO: сделать параметр from в find_word, чтобы сделать её линейной.
-	while( 1 )
-	{
-		uint64 start = find_word( f, word );
-		if( start == 0 )
-		{
+	while (1) {
+		uint64 start = find_word(f, word);
+		if (start == 0) {
 			// Если нет слова, и мы его не удаляли
 
 			return count;
@@ -85,72 +72,68 @@ int remove_words( FILE * f, char * word )
 		struct entry_t entry;
 		// TODO: дважды read_entry -- плохо
 		uint64 index;
-		index = read_entry( f, &entry, start, READ_ENTRY_ALL );
+		index = read_entry(f, &entry, start, READ_ENTRY_ALL);
 
-		mark_deleted_entry( &entry );
+		mark_deleted_entry(&entry);
 		count++;
 
-		int code = fseek( f, start, SEEK_SET );
-		if( 0 != code )
+		int code = fseek(f, start, SEEK_SET);
+		if (0 != code)
 			WTF();
 
-		write_entry( f, &entry);
+		write_entry(f, &entry);
 		header.actual_words--;
-		write_header( &header, f );
+		write_header(&header, f);
 	}
 }
 
-void command_remove( FILE * f )
-{
+void command_remove(FILE * f) {
 	char word[130];
-	read_word( word, 130 );
+	read_word(word, 130);
 
-	int count = remove_words( f, word );
-	if( count == 0 )
-		printf( "[INFO] Нет такого слова.\n" );
-	else if( count == 1 )
-		printf( "[INFO] Слово удалено.\n" );
+	int count = remove_words(f, word);
+	if (count == 0)
+		printf("[INFO] Нет такого слова.\n");
+	else if (count == 1)
+		printf("[INFO] Слово удалено.\n");
 	else
-		printf( "[WARNING] Удалено %d вхождений.\n", count );
+		printf("[WARNING] Удалено %d вхождений.\n", count);
 
 }
 
-void command_defragment( FILE * f )
-{
-    uint64 read_index, write_index, index;
-    read_index = write_index = jump_to_first_word( f );
-    int read_count = 0;
-    int write_count = 0;
-    struct entry_t entry;
-    again:
-    if( write_count < header.actual_words )
-    {
-        index = read_entry( f, &entry, read_index, READ_ENTRY_ALL );
-        if( existent_entry( &entry ) )
-        {
-            read_index = index;
-            read_count++;
-            int code = fseek( f, write_index, SEEK_SET );
-            if( 0 != code )
-                WTF();
-            write_entry( f, &entry );
-            write_count++;
-            write_index += entry_size( &entry );
-            goto again;
-        }
-        else
-        {
-            read_index = index;
-            goto again;
-        }
-    }
-    else
-    {
-        header.total_words = header.actual_words;
-        write_header( &header, f );
-        /*int code = fseek( f, write_index, SEEK_SET );
-        if( 0 != code )
-            WTF();*/
-        ftruncate( fileno( f ), write_index );
-    }
+void command_defragment(FILE * f) {
+	uint64 read_index, write_index, index;
+	read_index = write_index = jump_to_first_word(f);
+	int read_count = 0;
+	int write_count = 0;
+	struct entry_t entry;
+	again: if (write_count < header.actual_words) {
+		index = read_entry(f, &entry, read_index, READ_ENTRY_ALL);
+		if (existent_entry(&entry)) {
+			read_index = index;
+			read_count++;
+			int code = fseek(f, write_index, SEEK_SET);
+			if (0 != code)
+				WTF();
+			write_entry(f, &entry);
+			write_count++;
+			write_index += entry_size(&entry);
+			goto again;
+		} else {
+			read_index = index;
+			goto again;
+		}
+	} else {
+		header.total_words = header.actual_words;
+		write_header(&header, f);
+		ftruncate(fileno(f), write_index);
+	}
+}
+
+void command_help() {
+	printf("add\tДобавить слово.\n");
+	printf("find\tНайти слово.\n");
+	printf("del\tУдалить слово.\n");
+	printf("frag\tДефрагментация.\n");
+	printf("quit\tВыход и запись базы.\n");
 }
--- main.c
+++ main.c
@@ -3,300 +3,249 @@
 #include "entry.h"
 
 /*
-План:
-1. Хэширование (первые четыре символа, например).
-2. Собрать все хэши в одном месте.
-3. Собрать все хэши, слова и статьи в отдельных страницах (4096 - 65536 байт каждая).
-4. Сортировать хэши и слова в страницах.
-5. Сделать бинарное отсортированное дерево для хэшей.
-*/
+ План:
+ 1. Хэширование (первые четыре символа, например).
+ 2. Собрать все хэши в одном месте.
+ 3. Собрать все хэши, слова и статьи в отдельных страницах (4096 - 65536 байт каждая).
+ 4. Сортировать хэши и слова в страницах.
+ 5. Сделать бинарное отсортированное дерево для хэшей.
+ */
 
 #define DEFAULT_DB_NAME		"default.db"
 
-void check()
-{
-	if( 4 != sizeof( uint32 ) )
-	{
-		fprintf( stderr, "Размер uint32 не 32 бита, а %d. Надо поправить.\n", sizeof( uint32 ) );
-		exit( EXIT_WRONG_SIZEOF );
+void check() {
+	if (4 != sizeof( uint32 )) {
+		fprintf( stderr, "Размер uint32 не 32 бита, а %d. Надо поправить.\n",
+				sizeof( uint32 ));
+		exit( EXIT_WRONG_SIZEOF);
 	}
 
-	if( 8 != sizeof( uint64 ) )
-	{
-		fprintf( stderr, "Размер uint64 не 64 бита, а %d. Надо поправить.\n", sizeof( uint64 ) );
-		exit( EXIT_WRONG_SIZEOF );
+	if (8 != sizeof( uint64 )) {
+		fprintf( stderr, "Размер uint64 не 64 бита, а %d. Надо поправить.\n",
+				sizeof( uint64 ));
+		exit( EXIT_WRONG_SIZEOF);
 	}
 }
 
 // Формат базы (little endian).
 
-
-
-void write_header( struct header_t * header, FILE * f )
-{
-	int code = fseek( f, 0, SEEK_SET );
-	if( code == -1 )
+void write_header(struct header_t * header, FILE * f) {
+	int code = fseek(f, 0, SEEK_SET);
+	if (code == -1)
 		WTF();
 
-	code = fwrite( header, HEADER_SIZE, 1, f );
-	if( code != 1 )
+	code = fwrite(header, HEADER_SIZE, 1, f);
+	if (code != 1)
 		WTF();
 }
 
-void write_new_header( struct header_t * header, FILE * f )
-{
+void write_new_header(struct header_t * header, FILE * f) {
 	header->version = 1;
 	header->total_words = 0;
 	header->actual_words = 0;
 
 	int code;
-	write_header( header, f );
-	fflush( f );
+	write_header(header, f);
+	fflush(f);
 
-	code = ftruncate( fileno( f ), HEADER_SIZE );
-	if( 0 != code )
-	{
-		fprintf( stderr, "errno = %d\n", errno );
+	code = ftruncate(fileno(f), HEADER_SIZE);
+	if (0 != code) {
+		fprintf( stderr, "errno = %d\n", errno);
 		WTF();
 	}
 }
 
 char * db_filename = NULL;
 
-void read_header( struct header_t * header, FILE * f )
-{
+void read_header(struct header_t * header, FILE * f) {
 	int code;
-	code = fseek( f, 0, SEEK_SET );
-	if( code == -1 )
+	code = fseek(f, 0, SEEK_SET);
+	if (code == -1)
 		WTF();
 
-	code = fread( header, HEADER_SIZE, 1, f );
-	if( 1 != code )
+	code = fread(header, HEADER_SIZE, 1, f);
+	if (1 != code)
 		WTF();
 }
 
 struct header_t header;
 
-void save_database( FILE * f )
-{
-	write_header( &header, f );
+void save_database(FILE * f) {
+	write_header(&header, f);
 //	TODO( "save_database\n" );
 }
 
-void remove_newline( char * str )
-{
+void remove_newline(char * str) {
 	char * p = str;
-	while( *p )
+	while (*p)
 		p++;
 	p[-1] = 0;
 }
 
-uint64 jump_to_first_word( FILE * f )
-{
+uint64 jump_to_first_word(FILE * f) {
 	long offset = HEADER_SIZE;
-	int code = fseek( f, offset, SEEK_SET );
-	if( 0 != code )
+	int code = fseek(f, offset, SEEK_SET);
+	if (0 != code)
 		WTF();
 	return offset;
 }
 
 /*
-	Находит смещение, где находится запись для этого слова.
-	TODO: функции работающие с файлами 32-битные.
-*/
-uint64 find_word( FILE * f, char * pattern )
-{
+ Находит смещение, где находится запись для этого слова.
+ TODO: функции работающие с файлами 32-битные.
+ */
+uint64 find_word(FILE * f, char * pattern) {
 	int i;
 	int code;
-	uint64 offset = jump_to_first_word( f );
+	uint64 offset = jump_to_first_word(f);
 	int start;
 
 	struct entry_t entry;
-	for( i=0; i<header.total_words; i++ )
-	{
+	for (i = 0; i < header.total_words; i++) {
 		start = offset;
 		// TODO: совсем не обязательно выделять память здесь
-		offset = read_entry( f, &entry, start, READ_ENTRY_WORD | READ_ENTRY_DO_SEEK );
+		offset = read_entry(f, &entry, start,
+				READ_ENTRY_WORD | READ_ENTRY_DO_SEEK);
 		int good = 0;
-		if( existent_entry( &entry ) )
-		{
-			good = strcmp( entry.word, pattern ) == 0;
-			free_entry( &entry );
+		if (existent_entry(&entry)) {
+			good = strcmp(entry.word, pattern) == 0;
+			free_entry(&entry);
 		}
 
-		if( good )
+		if (good)
 			return start;
 	}
 	return 0;
 }
 
-uint64 skip_to_end( FILE * f )
-{
+uint64 skip_to_end(FILE * f) {
 	int i;
 	int code;
-	int offset = jump_to_first_word( f );
+	int offset = jump_to_first_word(f);
 	int start;
 
 	struct entry_t entry;
-	for( i=0; i<header.total_words; i++ )
-	{
+	for (i = 0; i < header.total_words; i++) {
 		start = offset;
 		// TODO: совсем не обязательно выделять память здесь
-		offset = read_entry( f, &entry, start, 0 );
-		free_entry( &entry );
+		offset = read_entry(f, &entry, start, 0);
+		free_entry(&entry);
 	}
 	return offset;
 }
 
-
-
-void add_word( FILE * f, char * word, char * content )
-{
-	skip_to_end( f );
+void add_word(FILE * f, char * word, char * content) {
+	skip_to_end(f);
 	struct entry_t entry;
-	entry.word_len = strlen( word );
-	entry.content_len = strlen( content );
+	entry.word_len = strlen(word);
+	entry.content_len = strlen(content);
 	entry.word = word;
 	entry.content = content;
-	write_entry( f, &entry );
+	write_entry(f, &entry);
 	header.total_words++;
 	header.actual_words++;
-	write_header( &header, f );
+	write_header(&header, f);
 //	TODO( "Вставить слово." );
 }
 
-int main( int argc, char ** argv )
-{
+int main(int argc, char ** argv) {
 	check();
 
 	// Передан файл базы как параметр
-	if( argc == 2 )
-	{
+	if (argc == 2) {
 		db_filename = argv[1];
 	}
 	// Файл не передан, используется имя по умолчанию
-	else if( argc == 1 )
-	{
+	else if (argc == 1) {
 		db_filename = "default.db";
 	}
 	// Неправильное использование
-	else
-	{
-		fprintf( stderr, "Usage: dictionary [words.db]\n" );
-		exit( EXIT_WRONG_USAGE );
+	else {
+		fprintf( stderr, "Usage: dictionary [words.db]\n");
+		exit( EXIT_WRONG_USAGE);
 	}
 
-	assert( NULL != db_filename );
+	assert(NULL != db_filename);
 
 	FILE * f = NULL;
-	int code = access( db_filename, R_OK | W_OK );
+	int code = access(db_filename, R_OK | W_OK);
 	// Нет прав, не существует файл или что-нибудь ещё
-	if( -1 == code )
-	{
+	if (-1 == code) {
 		// Нет файла, можно создавать
-		if( errno == ENOENT )
-		{
-			f = fopen( db_filename, "w+" );
-			fprintf( stderr, "INFO: Файл не существовал и теперь создан.\n" );
-		}
-		else if( errno == EACCES )
-		{
-			fprintf( stderr, "FATAL: Нет прав доступа к базе.\n" );
-			exit( EXIT_WRONG_RIGHTS );
-		}
-		else
-		{
-			printf( "errno = %d\n", errno );
-			exit( EXIT_WTF );
+		if ( errno == ENOENT) {
+			f = fopen(db_filename, "w+");
+			fprintf( stderr, "INFO: Файл не существовал и теперь создан.\n");
+		} else if ( errno == EACCES) {
+			fprintf( stderr, "FATAL: Нет прав доступа к базе.\n");
+			exit( EXIT_WRONG_RIGHTS);
+		} else {
+			printf("errno = %d\n", errno);
+			exit( EXIT_WTF);
 		}
 	}
 	// Есть права и файл существует
-	else
-	{
-		f = fopen( db_filename, "r+" );
-		fprintf( stderr, "INFO: Существующий файл открыт для работы.\n" );
+	else {
+		f = fopen(db_filename, "r+");
+		fprintf( stderr, "INFO: Существующий файл открыт для работы.\n");
 	}
-	assert( NULL != f );
+	assert(NULL != f);
 
-	fseek( f, 0, SEEK_END );
-	int length = ftell( f );
-	fprintf( stderr, "DEBUG: Длина файла: %d\n", length );
+	fseek(f, 0, SEEK_END);
+	int length = ftell(f);
+	fprintf( stderr, "DEBUG: Длина файла: %d\n", length);
 
-
-	if( length < HEADER_SIZE )
-	{
+	if (length < HEADER_SIZE) {
 		// Длина файла заведомо меньше размера header. Пишем новый.
-		write_new_header( &header, f );
-	}
-	else
-	{
+		write_new_header(&header, f);
+	} else {
 		// Читаем
-		read_header( &header, f );
+		read_header(&header, f);
 
 		// Проверяем
-		if( header.version != 1 || header.actual_words > header.total_words )
-		{
-			fprintf( stderr, "FATAL: База данных (%s), вероятно, испорчена. Возможно, стоит её удалить.\n", db_filename );
+		if (header.version != 1 || header.actual_words > header.total_words) {
+			fprintf( stderr,
+					"FATAL: База данных (%s), вероятно, испорчена. Возможно, стоит её удалить.\n",
+					db_filename);
 			WTF();
 		}
 	}
 
 	// Всё есть. Можно работать.
-	printf( "Введите help для справки, quit для выхода.\n" );
+	printf("Введите help для справки, quit для выхода.\n");
 	int working = 1;
-	while( working )
-	{
-		printf( "> " );
+	while (working) {
+		printf("> ");
 
 		char str[6];
-		fgets( str, 6, stdin );
-		remove_newline( str );
-		if( strcmp( str, "help" ) == 0 )
-		{
-			printf( "add\tДобавить слово.\n" );
-			printf( "find\tНайти слово.\n" );
-			printf( "del\tУдалить слово.\n" );
-			printf( "frag\tДефрагментация.\n" );
-			printf( "quit\tВыход и запись базы.\n" );
-		}
-		else if( strcmp( str, "quit" ) == 0 ||
-				 strcmp( str, "exit" ) == 0 ||
-				 strcmp( str, "bye" ) == 0 )
-		{
-			save_database( f );
-			exit( EXIT_SUCCESS );
-		}
-		else if( strcmp( str, "add" ) == 0 )
-		{
-			command_add( f );
-		}
-		else if( strcmp( str, "find" ) == 0 )
-		{
-			command_find( f );
-		}
-		else if( strcmp( str, "del" ) == 0 ||
-				 strcmp( str, "rm" ) == 0 )
-		{
-			command_remove( f );
-		}
-		else if( strcmp( str, "frag" ) == 0 )
-		{
-			command_defragment( f );
-		}
-		else
-		{
-			fprintf( stderr, "ERROR: Неизвестная команда '%s'\n", str );
+		fgets(str, 6, stdin);
+		remove_newline(str);
+		if (strcmp(str, "help") == 0) {
+			command_help();
+		} else if (strcmp(str, "quit") == 0 || strcmp(str, "exit") == 0
+				|| strcmp(str, "bye") == 0) {
+			save_database(f);
+			exit( EXIT_SUCCESS);
+		} else if (strcmp(str, "add") == 0) {
+			command_add(f);
+		} else if (strcmp(str, "find") == 0) {
+			command_find(f);
+		} else if (strcmp(str, "del") == 0 || strcmp(str, "rm") == 0) {
+			command_remove(f);
+		} else if (strcmp(str, "frag") == 0) {
+			command_defragment(f);
+		} else {
+			fprintf( stderr, "ERROR: Неизвестная команда '%s'\n", str);
 		}
 	}
 
 	/*
-	Операции:
-	1. Добавить слово. Имя (128 байт), содержание (не ограничено) ->
-	2. Найти слово. Имя -> содержание
-	3. Удалить. Имя ->
-	*/
-	if( f != NULL )
-		fclose( f );
+	 Операции:
+	 1. Добавить слово. Имя (128 байт), содержание (не ограничено) ->
+	 2. Найти слово. Имя -> содержание
+	 3. Удалить. Имя ->
+	 */
+	if (f != NULL)
+		fclose(f);
 
 	return EXIT_SUCCESS;
 }
